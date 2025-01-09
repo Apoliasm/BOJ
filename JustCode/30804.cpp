20ec026ba71b399097a84936388e8dfe8e9fafac
Apoliasm <syjsir@gmail.com>
2025-01-09 23:19:41 +0900

    1/9 S2 30804 과일 탕후루

--- JustCode/30804.cpp
+++ JustCode/30804.cpp
@@ -1,65 +1,38 @@
 #include <iostream>
 #include <vector>
-
+#include <algorithm>
 using namespace std;
+
+
 vector<int> tanghuru;
+vector<int> counter;
 
-void inputTanghuru(int n){
-    for(int i = 0; i< n ; i++){
-        cin>>tanghuru[i];
-    }
-}
-
-int travel(int forward,int n){
-
+int twoPointer(int n){
+    int start = 0, end = 0;
     int maxLength = 0;
-    int currentLength = 1;
-    int continuousLength = 1;
-    int beforeFruitType = 0;
-    int currentFruitType = 0;
-    int nextFruitType = 0;
-    int currentIndex = 0;
-    
-    if(forward == 1) 
-        currentIndex = 0;
-    else
-        currentIndex = n-1;
-    int iterate = 1;
-    for(int i = 1 ; i< n ; i++){
-        beforeFruitType = currentFruitType;
-        currentFruitType = tanghuru[currentIndex];
-        nextFruitType = tanghuru[currentIndex + forward] ;
-        currentLength = continuousLength + 1;
-        if(nextFruitType == currentFruitType){
-            continuousLength += 1;
-        }
-        else if (nextFruitType == beforeFruitType){
-            continuousLength = 1;
-        }
-        else{
-            maxLength = max(maxLength, currentLength);
-            
-            continuousLength = 1;
-        }
-        currentIndex += forward;
-        
+    int front,back,length = 0;
+    while(start <= end && end < n){
+        counter[tanghuru[end]] += 1;
+        end++;
+        while( 10 - count(counter.begin(),counter.end(),0) > 2)
+            counter[tanghuru[start++]] -- ;
+        maxLength = max(maxLength,end-start);
     }
-    maxLength = max(maxLength, currentLength);
-    
+
     return maxLength;
 }
+
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
     int n;
     cin>>n;
-    tanghuru = vector<int> (n);
-    inputTanghuru(n);
-    int result =  max (travel(1,n),travel(-1,n));
-    cout <<result;
-}  
-/*
-11
-5 1 1 2 1 1 2 4 5 6 7
-*/ 
+    tanghuru = vector<int>(n);
+    counter = vector<int> (10,0);
+    for(int i = 0 ; i<n ; i ++){
+        cin>>tanghuru[i];
+    }
+    int result = twoPointer(n);
+    cout<<result;
+}
